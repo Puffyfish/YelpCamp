63a5a1b369ed3a711faedb10d81ccf9d587cdef4
Puffyfish <93864592+Puffyfish@users.noreply.github.com>
2023-04-17 23:33:43 +0200

    Added /reviews joi schema
    - added joi schema for validation
    - added a validation middleware for reviews
    - updated /reviews route
    - updated the review field in the /campground/:id page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,34 @@
+const { campgroundSchema, reviewSchema } = require('./errorSchema.js');
+const ExpressError = require('./utils/ExpressError');
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) { // isAuthenticated is a method in PASSPORT
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login')
     }
     next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  console.log('validateCampground:', error);
+  if (error) {
+      const msg = error.details.map(el => el.message).join(',');
+      console.log("validateCampground: " + msg);
+      throw new ExpressError(msg, 400)
+  } else {
+      next();
+  }
+}
+
+module.exports.validateReview = (req, res, next) => {
+  const {error} = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el=>el.message).join(',') // map to make a single string
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+  console.log("validateSchema middleware: " + msg);
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,38 +1,30 @@
 const express = require('express');
-const router = express.router({mergeParams: true});
+const router = express.Router({mergeParams: true});
+// so we will have access to the params based from campgrounds
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-const { reviewSchema} = require ('../schemas.js'); // i dont know what this is
 // variables that deal with errors
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
-const ObjectID = require('mongoose').Types.ObjectId;
 
-const validateReview = (req, res, next) => {
-  const {error} = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el=>el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-  console.log(msg);
-}
+const { isLoggedIn, validateCampground, validateReview } = require('../middleware');
 
-router.post('/', validateReview, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
   campground.reviews.push(review);
   await review.save();
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
   const { id, reviewId } = req.params;
   await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId}});
   await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/campgrounds/${campground._id}`);
-}))
-);
+  // res.redirect(`/campgrounds/${campground._id}`);
+  res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router
