56ebc261a84fc8d35d0e34a1331a1f07dc379b32
Puffyfish <93864592+Puffyfish@users.noreply.github.com>
2023-04-17 23:49:11 +0200

    Nested User's info under Campground
    - added author key as type objectId in the campground model

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Review = require('./review')
 
 // define a model for mongoose to easier interact with mongodb
 const Schema = mongoose.Schema
@@ -9,8 +10,28 @@ const CampgroundSchema = new Schema({
   image: String,
   price: Number,
   description: String,
-  location: String
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+      await Review.deleteMany({
+          _id: {
+              $in: doc.reviews
+          }
+      })
+  }
+})
 
 // 2 parameters:
 // name of model(capitalized & singular; will make a collection after (plural & lowercased)
